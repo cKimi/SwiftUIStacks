d7231d8ee35d16e43c92480d7b7ed8a01eee1c9e
Carlos Kimura <carloskimura@Carloss-MacBook-Pro.local>
2025-01-22 17:38:34 -0300

    initial project files

---
+++ SwiftUIStacks/SwiftUIStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStacksApp.swift
+//  SwiftUIStacks
+//
+//  Created by Carlos Kimura on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
