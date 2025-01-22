29a399998bb31341f638fec0fbfc702afaabd85a
Carlos Kimura <ckimura@gmail.com>
2025-01-22 18:09:44 -0300

    choose plan cards created

--- SwiftUIStacks/ContentView.swift
+++ SwiftUIStacks/ContentView.swift
@@ -10,15 +10,59 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HeaderView()
+            
+            HStack {
+                VStack {
+                    Text("Basic")
+                        .font(.system(.title, design: .rounded))
+                        .fontWeight(.black)
+                        .foregroundColor(.white)
+                    Text("$9")
+                        .font(.system(size: 40, weight: .heavy, design: .rounded))
+                        .foregroundColor(.white)
+                    Text("per month")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+                .padding(40)
+                .background(Color.purple)
+                .cornerRadius(10)
+                
+                VStack {
+                    Text("Pro")
+                        .font(.system(.title, design: .rounded))
+                        .fontWeight(.black)
+                    Text("$19")
+                        .font(.system(size: 40, weight: .heavy, design: .rounded))
+                    Text("per month")
+                        .font(.headline)
+                        .foregroundColor(.gray)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+                .padding(40)
+                .background(Color(red: 240/255, green: 240/255, blue: 240/255))
+                .cornerRadius(10)
+            }
+            .padding(.horizontal)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct HeaderView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 2) {
+            Text("Choose")
+                .font(.system(.largeTitle, design: .rounded))
+                .fontWeight(.black)
+            Text("Your Plan")
+                .font(.system(.largeTitle, design: .rounded))
+                .fontWeight(.black)
+        }
+    }
 }
