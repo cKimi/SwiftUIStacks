fa7f4a2e8302a4d136632ec795a86a497b27fe03
Carlos Kimura <ckimura@gmail.com>
2025-01-22 21:24:04 -0300

    team card created

--- SwiftUIStacks/ContentView.swift
+++ SwiftUIStacks/ContentView.swift
@@ -12,40 +12,34 @@ struct ContentView: View {
         VStack {
             HeaderView()
             
-            HStack {
-                VStack {
-                    Text("Basic")
-                        .font(.system(.title, design: .rounded))
-                        .fontWeight(.black)
+            HStack(spacing: 15) {
+                PricingView(title: "Basic", price: "$9", textColor: .white, bgColor: .purple)
+                ZStack {
+                    PricingView(title: "Pro", price: "$19", textColor: .black, bgColor: Color(red: 240/255, green: 240/255, blue: 240/255))
+                    
+                    Text("Best for designer")
+                        .font(.system(.caption, design: .rounded))
+                        .fontWeight(.bold)
                         .foregroundColor(.white)
-                    Text("$9")
-                        .font(.system(size: 40, weight: .heavy, design: .rounded))
-                        .foregroundColor(.white)
-                    Text("per month")
-                        .font(.headline)
-                        .foregroundColor(.white)
+                        .padding(5)
+                        .background(Color(red: 255/255, green: 183/255, blue: 37/255))
+                        .offset(x: 0, y: 87)
                 }
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
-                .padding(40)
-                .background(Color.purple)
-                .cornerRadius(10)
-                
-                VStack {
-                    Text("Pro")
-                        .font(.system(.title, design: .rounded))
-                        .fontWeight(.black)
-                    Text("$19")
-                        .font(.system(size: 40, weight: .heavy, design: .rounded))
-                    Text("per month")
-                        .font(.headline)
-                        .foregroundColor(.gray)
-                }
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
-                .padding(40)
-                .background(Color(red: 240/255, green: 240/255, blue: 240/255))
-                .cornerRadius(10)
             }
             .padding(.horizontal)
+            
+            ZStack {
+                PricingView(title: "Team", price: "$299", textColor: .white, bgColor: Color(red: 62/255, green: 63/255, blue: 70/255), icon: "wand.and.rays")
+                    .padding()
+                
+                Text("Perfect for teams with 20 members")
+                    .font(.system(.caption, design: .rounded))
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .padding(5)
+                    .background(Color(red: 255/255, green: 183/255, blue: 37/255))
+                    .offset(x: 0, y: 110)
+            }
         }
     }
 }
@@ -64,5 +58,39 @@ struct HeaderView: View {
                 .font(.system(.largeTitle, design: .rounded))
                 .fontWeight(.black)
         }
+    }
+}
+
+struct PricingView: View {
+    
+    var title: String
+    var price: String
+    var textColor: Color
+    var bgColor: Color
+    var icon: String?
+    
+    var body: some View {
+        VStack {
+            if let icon = icon {
+                Image(systemName: icon)
+                    .font(.largeTitle)
+                    .foregroundStyle(textColor)
+            }
+            
+            Text(title)
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.black)
+                .foregroundColor(textColor)
+            Text(price)
+                .font(.system(size: 40, weight: .heavy, design: .rounded))
+                .foregroundColor(textColor)
+            Text("per month")
+                .font(.headline)
+                .foregroundColor(textColor)
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+        .padding(40)
+        .background(bgColor)
+        .cornerRadius(10)
     }
 }
